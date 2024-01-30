c29aede1789f4d1313e028f484709b837ba08e0a
Rolando Mesa <rmesa@toolsgroup.com>
2024-01-30 10:47:21 +0100

    First commit: project structure and angular material installed

---
+++ projects/shared-components/src/lib/shared-components.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-shared-components',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      shared-components works!
+    </p>
+  `,
+  styles: ``
+})
+export class SharedComponentsComponent {
+
+}
